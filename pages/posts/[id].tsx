77263f403db8d85c7844edde478de84d3b16633c
aedanmc <mccallaedan@gmail.com>
2023-01-13 14:03:37 -0800

    Update [id].js to [id].tsx

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,9 +4,10 @@ import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import { GetStaticProps, GetStaticPaths} from 'next';
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const postData = await getPostData(params?.id as string);
     return {
       props: {
         postData,
@@ -14,19 +15,15 @@ export async function getStaticProps({ params }) {
     };
   }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async() => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
 }
-/**
- * 
- * @param {postData} 
- * @returns 
- */
-export default function Post({ postData }) {
+
+export default function Post({ postData }: {postData : {title: string, date: string, contentHtml: string}}) {
     return (
       <Layout>
         <Head>
