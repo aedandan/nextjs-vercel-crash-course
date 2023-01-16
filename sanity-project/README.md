d18642b41a0f0d84779c78153153f179d152ec07
aedanmc <mccallaedan@gmail.com>
2023-01-15 20:19:58 -0800

    Add sanity-project to repo

---
+++ sanity-project/README.md
@@ -0,0 +1,3 @@
+# Sanity Clean Content Studio
+
+This is a sample for integrating Sanity.io with Next.js and deploying it on the Vercel platform. This content studio instance of Sanity has a very small dataset of two entries to represent a small blog website.
---
+++ sanity-project/schemas/blogpost.ts
@@ -0,0 +1,38 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+    name: 'post',
+    title: 'Post',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+            validation: (rule) => rule.required(), 
+        }),
+        defineField({
+            name: 'content',
+            title: 'Content',
+            type: 'array',
+            of: [{ type: 'block' }],
+        }),
+        defineField({
+            name: 'date',
+            title: 'Date',
+            type: 'datetime',
+            initialValue: () => new Date().toISOString(),
+        }),
+        defineField({
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+              source: 'title',
+              maxLength: 96,
+              isUnique: (value, context) => context.defaultIsUnique(value, context),
+            },
+            validation: (rule) => rule.required(),
+          })
+    ]
+})
